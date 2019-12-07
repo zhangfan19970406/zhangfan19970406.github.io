c64fc9a7116b6ebb3cc83bdf9130e55aed138e13
e-zhangfan <https://zhangfan19970406@github.com>
2019-12-07 09:44:37 +0800

    测试2

---
+++ build/precache-manifest.cf4c94fd6c437a8b4b11edce4a8f8886.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6620179b65c3b502e33c6615641a5db",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aca8edcec26048c97df3",
+    "url": "./static/css/2.623a5086.chunk.css"
+  },
+  {
+    "revision": "f76e8cd73b4a9693b035",
+    "url": "./static/css/main.68680233.chunk.css"
+  },
+  {
+    "revision": "aca8edcec26048c97df3",
+    "url": "./static/js/2.31be07c1.chunk.js"
+  },
+  {
+    "revision": "f76e8cd73b4a9693b035",
+    "url": "./static/js/main.1369b8d9.chunk.js"
+  },
+  {
+    "revision": "bab9ed39e57ce3de8330",
+    "url": "./static/js/runtime-main.101ccb2f.js"
+  },
+  {
+    "revision": "1799bf3c596d1acf6a69f6a5b63697ec",
+    "url": "./static/media/icomoon.1799bf3c.woff"
+  },
+  {
+    "revision": "2db373cbdef60a56d389aeb2b806d4b6",
+    "url": "./static/media/iconfont.2db373cb.svg"
+  },
+  {
+    "revision": "7e752e7f06bfd5bfa5c96e2cd341aa77",
+    "url": "./static/media/iconfont.7e752e7f.woff"
+  },
+  {
+    "revision": "9b0b14769bcbb6a6bc343d8d35dae716",
+    "url": "./static/media/iconfont.9b0b1476.ttf"
+  },
+  {
+    "revision": "9fda8ba46e77ffe73a92f02547cd386a",
+    "url": "./static/media/iconfont.9fda8ba4.eot"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
